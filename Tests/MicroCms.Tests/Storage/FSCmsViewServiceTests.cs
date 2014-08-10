cc89086a84b8019ed9e4f714403325b15eacfff7
Jon Stelly <jonstelly@gmail.com>
2014-08-10 18:45:22 -0500

    - Add missing test

---
+++ Tests/MicroCms.Tests/Storage/FSCmsViewServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using MicroCms.Configuration;
+using MicroCms.Storage;
+
+namespace MicroCms.Tests.Storage
+{
+    public class FSCmsViewServiceTests : CmsViewServiceTests<FSCmsViewService>
+    {
+        private readonly DirectoryInfo _Directory = new DirectoryInfo(@".\" + Guid.NewGuid());
+
+        protected override void ConfigureViewService(ICmsConfigurator configurator)
+        {
+            configurator.UseFileSystemStorage(_Directory);
+        }
+    }
+}
